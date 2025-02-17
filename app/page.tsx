50416f9691fd6d5fbb855d8d066fc813ef4cc65a
mdad-elec <61062853+mdad-elec@users.noreply.github.com>
2025-02-18 02:52:47 +0400

    auth

--- app/page.tsx
+++ app/page.tsx
@@ -3,16 +3,17 @@
 import { useState, useEffect } from "react";
 import { generateClient } from "aws-amplify/data";
 import type { Schema } from "@/amplify/data/resource";
-import "./../app/app.css";
-import { Amplify } from "aws-amplify";
+import { Amplify, AuthUser } from "aws-amplify";
 import outputs from "@/amplify_outputs.json";
+import { withAuthenticator, Button } from "@aws-amplify/ui-react";
 import "@aws-amplify/ui-react/styles.css";
+import "./../app/app.css";
 
 Amplify.configure(outputs);
 
 const client = generateClient<Schema>();
 
-export default function App() {
+function App({ signOut, user }: { signOut: () => void; user: AuthUser }) {
   const [students, setStudents] = useState<Array<Schema["Student"]["type"]>>([]);
   const [teachers, setTeachers] = useState<Array<Schema["Teacher"]["type"]>>([]);
 
@@ -47,21 +48,30 @@ export default function App() {
 
   return (
     <main>
-      <h1>Students</h1>
+      <h1>Welcome, {user.username}</h1>
+      <Button onClick={signOut}>Sign Out</Button>
+
+      <h2>Students</h2>
       <button onClick={createStudent}>+ Add Student</button>
       <ul>
         {students.map((student) => (
-          <li key={student.id}>{student.name} - {student.email} - Class: {student.class}</li>
+          <li key={student.id}>
+            {student.name} - {student.email} - Class: {student.class}
+          </li>
         ))}
       </ul>
 
-      <h1>Teachers</h1>
+      <h2>Teachers</h2>
       <button onClick={createTeacher}>+ Add Teacher</button>
       <ul>
         {teachers.map((teacher) => (
-          <li key={teacher.id}>{teacher.name} - {teacher.email}</li>
+          <li key={teacher.id}>
+            {teacher.name} - {teacher.email}
+          </li>
         ))}
       </ul>
     </main>
   );
 }
+
+export default withAuthenticator(App);
