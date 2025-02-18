32c9a05519bf3d238afe2f0c91984d0a8cd5db32
mdad-elec <61062853+mdad-elec@users.noreply.github.com>
2025-02-18 05:22:18 +0400

    newnewnew

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,64 @@
-import type { AppProps } from 'next/app';
-import { Authenticator } from '@aws-amplify/ui-react';
-import { Amplify } from 'aws-amplify';
-import outputs from '@/amplify_outputs.json';
-import '@aws-amplify/ui-react/styles.css';
+"use client";
+
+import { useState, useEffect } from "react";
+import { generateClient } from "aws-amplify/data";
+import { useAuthenticator } from "@aws-amplify/ui-react";
+import type { Schema } from "@/amplify/data/resource";
+import "./../app/app.css";
+import { Amplify } from "aws-amplify";
+import outputs from "@/amplify_outputs.json";
+import "@aws-amplify/ui-react/styles.css";
 
 Amplify.configure(outputs);
 
-export default function App({ Component, pageProps }: AppProps) {
+const client = generateClient<Schema>();
+
+export default function App() {
+  const [students, setStudents] = useState<Array<Schema["Student"]["type"]>>([]);
+  const { signOut } = useAuthenticator();
+
+  function listStudents() {
+    client.models.Student.observeQuery().subscribe({
+      next: (data) => setStudents([...data.items]),
+    });
+  }
+
+  useEffect(() => {
+    listStudents();
+  }, []);
+
+  function createStudent() {
+    const name = window.prompt("Student name");
+    const email = window.prompt("Student email");
+    const className = window.prompt("Student class");
+    if (name && email && className) {
+      client.models.Student.create({
+        name,
+        email,
+        class: className
+      });
+    }
+  }
+
   return (
-    <Authenticator>
-      {({ signOut, user }) => (
-        <main>
-          <h1>Hello {user?.username}</h1>
-          <button onClick={signOut}>Sign out</button>
-          <Component {...pageProps} />
-        </main>
-      )}
-    </Authenticator>
+    <main>
+      <h1>Students</h1>
+      <button onClick={createStudent}>+ new student</button>
+      <ul>
+        {students.map((student) => (
+          <li key={student.id}>
+            {student.name} - {student.email} - {student.class}
+          </li>
+        ))}
+      </ul>
+      <div>
+        🥳 App successfully hosted. Try creating a new student.
+        <br />
+        <a href="https://docs.amplify.aws/nextjs/start/quickstart/nextjs-app-router-client-components/">
+          Review next steps of this tutorial.
+        </a>
+      </div>
+      <button onClick={signOut}>Sign out</button>
+    </main>
   );
-};
+}
